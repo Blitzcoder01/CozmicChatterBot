dbeee92f67b539a41f37482d25fc6276cda122f3
riya <saxenaritu1979@gmail.com>
2020-03-17 11:26:34 +0530

    creation of communication class which help to convert text to speech of bot at execution time also import various libraries of freetts

--- src/Bot_communication/BotCommunication.java
+++ src/Bot_communication/BotCommunication.java
@@ -1,5 +1,15 @@
 package Bot_communication;
-
+import com.sun.speech.freetts.Gender;
+import java.beans.PropertyVetoException;
+import java.util.Locale;
+import java.util.Scanner;
+import javax.speech.AudioException;
+import javax.speech.Central;
+import javax.speech.EngineException;
+import javax.speech.EngineStateError;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
+import javax.speech.synthesis.Voice;
 public class BotCommunication {
 
 }
