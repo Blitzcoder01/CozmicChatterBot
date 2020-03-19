d751ac8f6ff1e45e72949c1cfeb671c2070d7d16
riya <saxenaritu1979@gmail.com>
2020-03-19 11:38:07 +0530

    calling Runtime class by creating it's object and handling the exceptions

--- src/Dictionary/dictionary.java
+++ src/Dictionary/dictionary.java
@@ -5,13 +5,22 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 public class dictionary {
-    public void search_dictionary(){
+    public void search_dictionary() {
         String url = "https://dictionary.cambridge.org";
-        if(Desktop.isDesktopSupported()) {
+        if (Desktop.isDesktopSupported()) {
             Desktop desktop = Desktop.getDesktop();
             try {
                 desktop.browse(new URI(url));
             } catch (IOException | URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } else {
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("xdg-open " + url);
+            } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
