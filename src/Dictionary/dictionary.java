8575711871712e00016d201e5ac2d6e0ea35830b
riya <saxenaritu1979@gmail.com>
2020-03-19 11:30:10 +0530

    storing the url of the web page which going to open by this class

--- src/Dictionary/dictionary.java
+++ src/Dictionary/dictionary.java
@@ -6,6 +6,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 public class dictionary {
     public void search_dictionary(){
-
+        String url = "https://dictionary.cambridge.org";
     }
 }
