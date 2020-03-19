7282fe5627e2d5da0278e6c12f3c7d65dfaba3dd
riya <saxenaritu1979@gmail.com>
2020-03-19 11:52:27 +0530

    for open the stored url and handling the exceptions

--- src/TowerOfHanoi/TowerOfHanoi.java
+++ src/TowerOfHanoi/TowerOfHanoi.java
@@ -8,5 +8,14 @@ import java.net.URISyntaxException;
 public class TowerOfHanoi {
     public  void PlayTowerOfHanoi() {
         String url = "https://www.mathsisfun.com/games/towerofhanoi.html";
+        if(Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
