25ccd5f37294142ea3d237128fa349000d1d584e
riya <saxenaritu1979@gmail.com>
2020-03-19 11:58:07 +0530

    importing various classes and creation of WebSearch class

--- src/WebSearch/WebSearch.java
+++ src/WebSearch/WebSearch.java
@@ -6,24 +6,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class WebSearch {
-    public void search(){
-        String url = "http://www.google.com";
-        if(Desktop.isDesktopSupported()){
-            Desktop desktop = Desktop.getDesktop();
-            try {
-                desktop.browse(new URI(url));
-            } catch (IOException | URISyntaxException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }else{
-            Runtime runtime = Runtime.getRuntime();
-            try {
-                runtime.exec("xdg-open " + url);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
+
+
+
 }
