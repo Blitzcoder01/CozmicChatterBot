289ea366b013b16f5657da0a6b57395fcca2b9b0
riya <saxenaritu1979@gmail.com>
2020-03-19 11:45:12 +0530

    for open the stored url and handling the exceptions

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -7,6 +7,15 @@ import java.net.URISyntaxException;
 public class TicTacToe {
     public void playTicTacToe(){
         String url = "https://playtictactoe.org";
+        if(Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
 
     }
 }
