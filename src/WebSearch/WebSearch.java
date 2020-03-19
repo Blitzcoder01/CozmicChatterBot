1beb854ff31b4b9afb8d42278e62264489f142c0
riya <saxenaritu1979@gmail.com>
2020-03-19 12:01:44 +0530

    for open the stored url and handling the exceptions

--- src/WebSearch/WebSearch.java
+++ src/WebSearch/WebSearch.java
@@ -8,5 +8,14 @@ import java.net.URISyntaxException;
 public class WebSearch {
     public void search(){
         String url = "http://www.google.com";
+        if(Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
