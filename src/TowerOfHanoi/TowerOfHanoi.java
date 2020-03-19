4381ffb47e62deb907ffb235329c3a5b45447e9d
riya <saxenaritu1979@gmail.com>
2020-03-19 11:53:46 +0530

    calling Runtime class by creating it's object and handling the exceptions

--- src/TowerOfHanoi/TowerOfHanoi.java
+++ src/TowerOfHanoi/TowerOfHanoi.java
@@ -17,5 +17,14 @@ public class TowerOfHanoi {
                 e.printStackTrace();
             }
         }
+        else {
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("xdg-open " + url);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
