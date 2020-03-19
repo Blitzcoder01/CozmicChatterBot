0ac6c840181f99a2bb4ca87bc148a5e5d631de12
riya <saxenaritu1979@gmail.com>
2020-03-19 11:48:39 +0530

    importing various classes and creation of  TowerOfHanoi class

--- src/TowerOfHanoi/TowerOfHanoi.java
+++ src/TowerOfHanoi/TowerOfHanoi.java
@@ -6,24 +6,5 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class TowerOfHanoi {
-    public  void PlayTowerOfHanoi(){
-        String url = "https://www.mathsisfun.com/games/towerofhanoi.html";
-        if(Desktop.isDesktopSupported()){
-            Desktop desktop = Desktop.getDesktop();
-            try {
-                desktop.browse(new URI(url));
-            } catch (IOException | URISyntaxException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }else {
-            Runtime runtime = Runtime.getRuntime();
-            try {
-                runtime.exec("xdg-open " + url);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
+
 }
