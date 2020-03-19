a49c3cf13ccaf7273e7acfb21e239d47412ecd32
riya <saxenaritu1979@gmail.com>
2020-03-19 11:33:12 +0530

    for open the stored url and handling the exceptions

--- src/Dictionary/dictionary.java
+++ src/Dictionary/dictionary.java
@@ -7,5 +7,14 @@ import java.net.URISyntaxException;
 public class dictionary {
     public void search_dictionary(){
         String url = "https://dictionary.cambridge.org";
+        if(Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
