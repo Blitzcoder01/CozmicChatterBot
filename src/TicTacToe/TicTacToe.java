89b92d0f5a53a0197c67faf956101002f33b9244
riya <saxenaritu1979@gmail.com>
2020-03-19 11:40:22 +0530

    importing various classes and creation of TicTacToe class

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -5,24 +5,5 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class TicTacToe {
-    public void playTicTacToe(){
-        String url = "https://playtictactoe.org";
-        if(Desktop.isDesktopSupported()){
-            Desktop desktop = Desktop.getDesktop();
-            try {
-                desktop.browse(new URI(url));
-            } catch (IOException | URISyntaxException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }else {
-            Runtime runtime = Runtime.getRuntime();
-            try {
-                runtime.exec("xdg-open " + url);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
+
 }
